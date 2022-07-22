e4484f87e10a112d9261ffc298d7dcf2706adc21
abilbotz1 <108676663+abilbotz1@users.noreply.github.com>
2022-07-22 14:13:54 +0700

    Update bot-salken.js

--- plugins/bot-salken.js
+++ plugins/bot-salken.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
-	conn.sendButton(m.chat, `Halo Kak👋\nsaya adalah FangzBot, FangzBot adalah Sebuah Bot yang bisa membantumu di grup ini, klik tombol dibawah ini jika kamu ingin menggunakan bot!`.trim(), `${wm}`, '📮Menu', '.menu', m)
+	conn.sendButton(m.chat, `Halo Kak👋\nsaya adalah KURUSHI-Md, KURUSHI-Md adalah Sebuah Bot yang bisa membantumu di grup ini, klik tombol dibawah ini jika kamu ingin menggunakan bot!`.trim(), `${wm}`, '📮Menu', '.menu', m)
 	}
 
 handler.command = /^(salken)$/i
