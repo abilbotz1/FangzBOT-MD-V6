02314e99232a7e3f515d72aab425b5e7767ca8fa
abilbotz1 <108676663+abilbotz1@users.noreply.github.com>
2022-07-22 14:05:06 +0700

    Update info-ytbot.js

--- plugins/info-ytbot.js
+++ plugins/info-ytbot.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
- await conn.sendButton(m.chat, `YT: FANGZ BOT\n`,wm + '\n\n' + botdate, giflogo, [['OK','ok']], m, {
+ await conn.sendButton(m.chat, `KURUSHI BOT\n`,wm + '\n\n' + botdate, giflogo, [['OK','ok']], m, {
 contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
                         title: bottime,
-                        body: 'By Fangz',          previewType: 0,
+                        body: 'By Riel',          previewType: 0,
                         thumbnail: fs.readFileSync("./thumbnail.jpg"),
                         sourceUrl: https://youtube.com/channel/UCACHvReRmw2fxgMutPFCBWg
                       }}
