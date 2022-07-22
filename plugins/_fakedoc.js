3a4bdc25751b9b1805791f4cbe0251f6e6224f4b
abilbotz1 <108676663+abilbotz1@users.noreply.github.com>
2022-07-22 13:54:53 +0700

    Update _fakedoc.js

--- plugins/_fakedoc.js
+++ plugins/_fakedoc.js
@@ -10,7 +10,7 @@ const anu = {
                         },
        message: {
                     documentMessage: {
-                    title: 'YT: FANGZ GANZ', 
+                    title: 'RIEL GANZ', 
                     jpegThumbnail: './thumbnail.jpg'
                           }
                         }
